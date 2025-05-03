d8048fa28dc107b25df77a643d0462c087576e07
mollnardi04 <mollnardi04@gmail.com>
2025-05-03 23:39:13 +0300

    Fixing result of conflict resolve

--- calculator.py
+++ calculator.py
@@ -13,7 +13,5 @@ if __name__ == "__main__":
     num2 = 7
     print(f"{num1} + {num2} = {add(num1, num2)}")
     print(f"{num1} - {num2} = {subtract(num1, num2)}")
-    # Додаємо виклик нової функції
-    print(f"{num1} * {num2} = {multiply(num1, num2)}")
-    # Змінюємо повідомлення
-    print("Калькулятор оновлено: додано множення.") # Змінений рядок!
+    print(f"{num1} * {num2} = {multiply(num1, num2)}") # 3 feature/multiply
+    print("Виправлено текст повідомлення у main, інтегровано множення.") # Комбінований результат
